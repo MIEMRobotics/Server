67bd5c8512321a040a23790b4fcee9efe5575fe9
Armaliltril <aratornshadow@mail.ru>
2018-12-05 02:23:47 +0300

    Remade server
    Signed-off-by: Armaliltril <armaliltril@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_uploads import UploadSet, IMAGES, AUDIO, configure_uploads
+from flask_socketio import SocketIO, join_room, emit
+import speech_recognition
 
 path_to_images = r'./tests/responses'
 path_to_audios = r'./tests/responses'
@@ -8,6 +10,8 @@ app = Flask(__name__)
 app.config["UPLOADED_IMAGES_DEST"] = path_to_images
 app.config["UPLOADED_AUDIOS_DEST"] = path_to_audios
 app.secret_key = b'gOdSAvetHeroMaNOV\n\xec]/'
+
+socketio = SocketIO(app)
 
 images = UploadSet('images', IMAGES)
 audios = UploadSet('audios', AUDIO)
@@ -19,12 +23,27 @@ files_to_handlers = \
         'audio': audios
     }
 
+recognizer = speech_recognition.Recognizer()
+
 
 def upload(file_type: str):
     if file_type in request.files:
         files_to_handlers[file_type].save(request.files[file_type])
         return "Good"
     return "Bad"
+
+
+def classify_image(image):
+    return image
+
+
+def detect_image(image):
+    return image
+
+
+def audio_to_text(audio):
+    text = recognizer.recognize_google(audio)
+    return text
 
 
 @app.route('/')
@@ -40,3 +59,14 @@ def upload_image():
 @app.route('/upload_audio', methods=['POST'])
 def upload_audio():
     return upload("audio")
+
+
+@socketio.on("detect")
+def detect(data):
+    image = data['image']
+    detected = detect_image(image)
+    emit('detected', {'image': detected})
+
+
+if __name__ == '__main__':
+    app.run()
