8a02ac20b62754ec8a2160072296381193f574e1
Armaliltril <aratornshadow@mail.ru>
2019-05-21 21:43:01 +0300

    Changed app interface

--- app.py
+++ app.py
@@ -1,101 +1,176 @@
 from flask import Flask, request
 from flask_uploads import UploadSet, IMAGES, AUDIO, configure_uploads
 from flask_socketio import SocketIO, join_room, emit, send
-import speech_recognition
-from utils.uploader import *
 from utils.speech import *
-
-path_to_images = r'./tests/responses'
-path_to_audios = r'./tests/responses'
-
-app = Flask(__name__)
-app.config["UPLOADED_IMAGES_DEST"] = path_to_images
-app.config["UPLOADED_AUDIOS_DEST"] = path_to_audios
-app.secret_key = b'gOdSAvetHeroMaNOV\n\xec]/'
-
-socketio = SocketIO(app)
-
-images = UploadSet('images', IMAGES)
-audios = UploadSet('audios', AUDIO)
-configure_uploads(app, (images, audios,))
-
-files_to_handlers = \
-    {
-        'image': images,
-        'audio': audios
-    }
-
-recognizer = speech_recognition.Recognizer()
+from types import FunctionType
 
 
-def upload(file_type: str):
-    if file_type in request.files:
-        files_to_handlers[file_type].save(request.files[file_type])
-        return "Good"
-    return "Bad"
+class ServerApp:
+    """
+    Basic class from you can inherit from or use functional API to extend it
+    """
+
+    def __init__(self, secret_key=b'1'):
+        """
+        Initialize app
+
+        :param secret_key: Secret key for your app
+        :type secret_key: bytearray
+        """
+
+        self._flask_app = Flask(__name__)
+        self._flask_app.secret_key = secret_key
+
+        self._socket_app = SocketIO(self._flask_app)
+
+        @self._socket_app.on('test')
+        def get_response():
+            """
+            Test function
+
+            :return: "Hello, World"
+            :rtype: str
+            """
+            return "Hello, World!"
+
+    def run(self, host, port):
+        """
+        Runs the server
+
+        :param host: ip of your server. Use 0.0.0.0 to open to outer connection
+        :type host: str
+        :param port: port of your server. Must be open.
+        :type port: int
+        :return: None
+        :rtype: None
+        """
+
+        self._socket_app.run(host=host, port=port, app=self._flask_app)
+
+    def add_socket_function(self, func, message):
+        """
+        Adds socket based function to your API.
+
+        :param func: function object
+        :type func: FunctionType
+        :param message: signal that trigger function
+        :type message: str
+
+        :return: None
+        :rtype: None
+
+        Example::
+
+            def get_up_text(text: str):
+                return text.upper()
+
+            app = ServerApp()
+            app.add_socket_function(get_some_text, 'up')
+        """
+
+        @self._socket_app.on(message)
+        def new_func(*args):
+            return func(*args)
+        new_func.__name__ = func.__name__
+
+    def add_route_function(self, func, route):
+        """
+        Adds http requests based function to your API.
+
+        :param func: function object
+        :type func: FunctionType
+        :param route: route that trigger function
+        :type route: str
+
+        :return: None
+        :rtype: None
+
+        Example::
+
+            def get_up_text(text: str):
+                return text.upper()
+
+            app = ServerApp()
+            app.add_route_function(get_some_text, 'up')
+        """
+        @self._flask_app.route(route)
+        def new_func(*args):
+            return func(*args)
+        new_func.__name__ = func.__name__
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class DialogApp(ServerApp):
+
+    def __init__(self):
+
+        super().__init__()
+
+        @self._socket_app.on("give_answer")
+        def get_answer(data):
+            """
+            Answers the question using DialogFlow API
+
+            :param data: dict with 2 fields - 'audio' and 'return_type'.
+             'audio' is wav file, coded as hex  string.
+             'return_type' is 'text' or 'audio'
+
+            :return: audio file, coded as hex string if 'return_type' is 'audio'.
+              text string if 'return_type' is 'text'
+            :rtype: str
+            """
+            question = audio_to_text(bytearray.fromhex(data['audio']))
+            answer = question_to_answer(question)
+
+            if data['return_type'] == "text":
+                return answer
+
+            elif data['return_type'] == "audio":
+                audio = text_to_audio(answer)
+                return bytearray.hex(audio)
+            else:
+                return None
+
+        @self._socket_app.on("audio_to_text")
+        def convert_audio(audio):
+            """
+            Maket audio to text transform and returns it as callback
+
+            :param audio: audio file in wav format, coded as hex string
+            :type audio: str
+
+            :return: Text in the recording
+            :rtype: str
+            """
+            text = audio_to_text(bytearray.fromhex(audio))
+            return text
+
+        @self._socket_app.on("text_to_audio")
+        def convert_text(text):
+            """
+            Make text to audio transform and returns as callback
+
+            :param text: text request
+            :type text: str
+
+            :return: audio file in .wav format, coded as hex string
+            :rtype: str
+            """
+            audio = text_to_audio(text)
+            return bytes.hex(audio)
 
 
-@socketio.on("connect")
-def connect():
-    return True
-
-
-@app.route('/upload_image', methods=['POST'])
-def upload_image():
-    return upload("image")
-
-
-@app.route('/upload_audio', methods=['POST'])
-def upload_audio():
-    return upload("audio")
-
-
-@socketio.on('test')
-def test(data):
-    return data
-
-
-@socketio.on('upload_image')
-def upload_image(image):
-    return upload("image")
-
-
-@socketio.on("upload_audio")
-def upload_audio(audio):
-    return upload("audio")
-
-
-@socketio.on("give_answer")
-def get_answer(data):
-    question = audio_to_text(bytearray.fromhex(data['audio']))
-    answer = question_to_answer(question)
-
-    if data['return_type'] == "text":
-        return answer
-
-    elif data['return_type'] == "audio":
-        audio = text_to_audio(answer)
-        return bytearray.hex(audio)
-    else:
-        return None
-
-
-@socketio.on("audio_to_text")
-def convert(audio):
-    text = audio_to_text(bytearray.fromhex(audio))
+def get_some_text(text):
     return text
 
 
-@socketio.on("text_to_audio")
-def convert(text):
-    audio = text_to_audio(text)
-    return bytes.hex(audio)
+def get_up_text(text: str):
+    return text.upper()
 
 
 if __name__ == '__main__':
-    socketio.run(host='0.0.0.0', port=5000)
+
+    app = DialogApp()
+    app.add_socket_function(get_some_text, 'text')
+    app.add_socket_function(get_some_text, 'up')
+
+    app.run('127.0.0.1', 5000)
